f898651723c7df39420357f5516dd23209517f59
Karolis <zimantask@gmail.com>
2020-06-08 23:11:02 +0300

    local files

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def coin_count(amount=75, coins=[1, 4, 15, 20]):
+    'returned from a given sum amount in the number of coins'
+    remaining_amount = amount
+    denomination = []
+    counts = []
+
+    #creating list of used coins and counts
+    for coin in sorted(coins, reverse=True):
+        num = remaining_amount / coin
+        if num:
+            denomination.append((coin))
+            count_id = remaining_amount // coin
+            counts.append(count_id)
+            remaining_amount -= count_id * coin
+            final_dict = {}
+            dict_indx = 0
+            for value in denomination:
+                final_dict[value] = counts[dict_indx]
+                dict_indx += 1
+
+    # deleting zero value keys
+    for key, value in list(final_dict.items()):
+        if value == 0:
+            del final_dict[key]
+    return final_dict
+
+if __name__ == '__main__':
+    app.run(debug=True)
